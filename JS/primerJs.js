afd9f41a72dfa1ddeb2d73ba541d340051659d79
brauliogm <brauliogm16@gmail.com>
2023-08-11 08:18:20 -0600

    Listos los fundamentos de la sintaxis de JS

---
+++ JS/primerJs.js
@@ -0,0 +1,12 @@
+function nombreDeFuncion(nombreParametro) {
+    alert("Primer Hola mundo! de " + nombreParametro + 
+    " entendiendo las funciones en JS");
+}
+
+nombreDeFuncion("Braulio");
+
+function suma(a, b) {
+    return a + b;
+}
+
+console.log(suma(3, 6));
