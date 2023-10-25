9b145a30edb9672650be3f24e1e627650c0975bd
brauliogm <brauliogm16@gmail.com>
2023-10-25 07:57:34 -0600

    Metodo para autenticar el usuario

--- Angular/applicacion-final/src/app/login/login.component.ts
+++ Angular/applicacion-final/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Login } from '../modelos/login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,20 @@ export class LoginComponent {
 
   login: Login = new Login();
 
+  constructor(private http: HttpClient){}
+
   autenticar(){
-    console.log(this.login);
-    
+    let usuario = this.login.username + ":" + this.login.password;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': 'Basic' + btoa(usuario)
+      })
+    }
+    this.http.get("/api/v1/login", httpOptions).subscribe(
+      (data)=>{
+        console.log(data);
+      }
+    )
   }
 }
