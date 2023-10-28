40a629676c3526d934a97fbc6136f2f0e0eddaed
brauliogm <brauliogm16@gmail.com>
2023-10-27 20:43:10 -0600

    Terminando el componente de libros para agregar y mostrar los libros

--- Angular/applicacion-final/src/app/libros/libros.component.ts
+++ Angular/applicacion-final/src/app/libros/libros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Libro } from '../modelos/libro';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-libros',
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class LibrosComponent {
   libros: any;
   agregando: any;
-  libro: Libro;
+  libro!: Libro;
 
   constructor(private router:Router, private http:HttpClient){
 
@@ -20,10 +20,47 @@ export class LibrosComponent {
   ngOnInit(){
     this.libro = {} as Libro;
     this.agregando = false;
+    let up = localStorage.getItem("username") + ":" + localStorage.getItem("password");
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': 'Basic ' + btoa(up)
+      })
+    }
+    this.http.get("/api/v1/libros", httpOptions).subscribe(
+      (data)=>{
+        this.libros = data;
+        console.log(data);
+        
+      },
+      (error) => {
+        console.error('Error en la solicitud:', error);
+      }
+    )
   }
 
   agregar(){
     this.agregando = !this.agregando;
   }
 
+  agregarLibro(){
+    let up = localStorage.getItem("username") + ":" + localStorage.getItem("password");
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': 'Basic ' + btoa(up)
+      })
+    };
+    this.http.post("/api/v1/libros", this.libro, httpOptions).subscribe(
+      (data)=>{
+        this.libro.autor = '';
+        this.libro.titulo = '';
+        this.agregar();
+        this.ngOnInit();
+      },
+      (error) => {
+        console.error('Error en la solicitud:', error);
+      }
+    )
+  }
 }
